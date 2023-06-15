3e0a77af849de2622600409cc1a422d5da090949
Quadr <awequadr15@gmail.com>
2023-06-15 13:44:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,7 @@
-#include <stido.h>
+#include <stdio.h>
 
-/**
- * main - Entry point 
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+int main() {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
 }
 
