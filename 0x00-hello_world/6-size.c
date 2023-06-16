b4ba5caf034cae5137b039f1074c856cafc4798a
Quadr <awequadr15@gmail.com>
2023-06-16 08:24:35 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 /**
-i* main - Entry point
+* main - Entry point
 *
 * Return Always 0 (Success)
 */
+
 int main(void)
 {
-printf("Size of a char: %lu bbyte(s)\n", sizeof(char));
-printf("Size of an int %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float %lu byte(s)\n", size(float));
-return (0);}
+printf("Size of char: %zu byte(s)\n", sizeof(char));
+printf("Size of int: %zu byte(s)\n", sizeof(int));
+printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of float: %zu byte(s)\n", sizeof(float));
+
+return (0);
+}
