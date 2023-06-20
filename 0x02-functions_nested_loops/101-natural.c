2deb332e66f93b7c9bae05029b3f85a53d44fe0f
Quadr <awequadr15@gmail.com>
2023-06-20 10:01:55 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -14,15 +14,18 @@ unsigned long fib1 = 0, fib2 = 1, sum;
 for (count = 0; count < 50; count++)
 {
 sum = fib1 + fib2;
-printf("%lu", sum);
+
+if (count < 49)
+{
+printf("%lu, ", sum);
+}
+else
+{
+printf("%lu\n", sum);
+}
 
 fib1 = fib2;
 fib2 = sum;
-
-if (count == 49)
-printf("\n");
-else
-printf(", ");
 }
 
 return (0);
