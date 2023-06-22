922d1d9cac7393a262b1044cba697d9c88684545
Quadr <awequadr15@gmail.com>
2023-06-22 10:42:12 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,24 +2,19 @@
 
 
 /**
-
- * _isupper - uppercase letters
-
- * @c: char to check
-
- *
-
- * Return: 0 or 1
-
+ * _isupper - Checks for lowercase character
+ * @c:The character to be checked
+ * Return: 1 for uppercase character or 0 for anything else
  */
 
 
 int _isupper(int c)
 
 {
-if (c >= 'A' && c <= 'Z')
+if (c >= 65 && c <= 90)
+{
 return (1);
-else
+}
 return (0);
 }
 
