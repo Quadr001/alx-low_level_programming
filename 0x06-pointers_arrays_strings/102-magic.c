184498e4e8da04cf09a6b9974f3f7b047a4da12c
Quadr <103822802+Quadr001@users.noreply.github.com>
2023-06-29 18:46:04 +0100

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+* main - Entry point of the program
+*
+* Return: 0 on success
+*/
 int main(void)
 {
 int n;
@@ -7,14 +12,9 @@ int a[5];
 int *p;
 a[2] = 1024;
 p = &n;
-
 /*
 * write your line of code here...
 * Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
 */
 *(p + 5) = 98;
 
