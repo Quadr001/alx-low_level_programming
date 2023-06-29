376354d9625a068bcf272ab471b5789a17913906
Quadr <103822802+Quadr001@users.noreply.github.com>
2023-06-29 17:34:58 +0100

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,16 +7,17 @@
  */
 int main(void)
 {
-	int a[5] = {0, 1, 2, 3, 4};
-	int *p;
+int a[5] = {0, 1, 2, 3, 4};
+int *p;
 
-	p = &a[2];
+p = &a[2];
 
-	/* Add your code below this line */
-	printf("a[2] = %d\n", *(p + 0));  /* New line of code */
-	/* Add your code above this line */
+/* Add your code below this line */
+printf("a[2] = %d\n", *(p + 0));
+/* Add your code above this line */
 
-	a[2] = 98;
-	printf("%d\n", a[2]);
-	return (0);
+a[2] = 98;
+	
+printf("%d\n", a[2]);
+return (0);
 }
