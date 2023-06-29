3fae2ee02f5f2fe8cac4942ba6f2d7b7d0ea07a7
Quadr <103822802+Quadr001@users.noreply.github.com>
2023-06-29 17:47:29 +0100

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
 int main(void)
+
 {
-int a[5] = {0, 1, 2, 3, 4};
-int *p;
+ int n;
+ int a[5];
+ int *p;
 
-p = &a[2];
+ a[2] = 1024;
+ p = &n;
 
+    
 /* Add your code below this line */
-*(p + 0) = 98;  /* New line of code */
+*(p + (sizeof(n) / sizeof(*p)) + 1) = 98;
 /* Add your code above this line */
 
+   
 printf("a[2] = %d\n", a[2]);
-	
+    
 return 0;
 }
