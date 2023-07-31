d4201fefaf60467e5488853e8910e24ba2b0d871
Quadr <103822802+Quadr001@users.noreply.github.com>
2023-07-31 22:01:46 +0100

    102-free_listint_safe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,36 +1,33 @@
+#include <stddef.h>
 #include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a listint_t linked list
- * @h: Double pointer to the head node of the linked list
- *
- * Return: The size of the list that was freed
- */
+* free_listint_safe - Frees a listint_t linked list.
+* @h: Double pointer to the head of the list.
+*
+* Return: The size of the list that was freed.
+*/
 size_t free_listint_safe(listint_t **h)
 {
-    size_t count = 0;
-    listint_t *current, *temp;
+listint_t *current, *temp;
+size_t count = 0;
+if (h == NULL || *h == NULL)
+return (0);
 
-    if (!h || !*h)
-        return 0;
-
-    current = *h;
-    while (current)
-    {
-        count++;
-
-        if (current->next >= current)
-        {
-            *h = NULL;
-            return count;
-        }
-
-        temp = current;
-        current = current->next;
-        free(temp);
-    }
-
-    *h = NULL;
-    return count;
+current = *h;
+while (current)
+{
+count++;
+if (current <= current->next)
+{
+*h = NULL; /* Set the head to NULL before freeing */
+return (count);
+}
+temp = current;
+current = current->next;
+free(temp);
+}
+*h = NULL; /* Set the head to NULL after freeing */
+return (count);
 }
