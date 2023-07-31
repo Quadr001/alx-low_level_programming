1a26a8610c1495bf9e7d52e0996ecee477b5d942
Quadr <103822802+Quadr001@users.noreply.github.com>
2023-07-31 21:45:42 +0100

    101-print_listint_safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,28 +3,36 @@
 #include "lists.h"
 
 /**
-* print_listint_safe - Prints a listint_t linked list and returns the number of nodes
-* @head: Pointer to the first node of the linked list
-*
-* Return: The number of nodes in the list
-*/
+ * print_listint_safe - Prints a listint_t linked list and returns the number of nodes
+ * @head: Pointer to the first node of the linked list
+ *
+ * Return: The number of nodes in the list
+ */
 size_t print_listint_safe(const listint_t *head)
 {
 size_t num_nodes = 0;
-const listint_t *current = head;
-
-while (current)
+const listint_t *slow = head, *fast = head;
+while (slow && fast && fast->next)
 {
-printf("[%p] %d\n", (void *)current, current->n);
+slow = slow->next;
+fast = fast->next->next;
+/* Check for cycle (fast pointer catches up to slow pointer) */
+if (slow == fast)
+{
+printf("[%p] %d\n", (void *)slow, slow->n);
 num_nodes++;
-/* Check if the next node points back to a previous node (infinite loop) */
-if (current >= current->next)
+slow = slow->next;
+while (slow != fast)
 {
-printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+printf("[%p] %d\n", (void *)slow, slow->n);
+num_nodes++;
+slow = slow->next;
+}
+printf("-> [%p] %d\n", (void *)slow, slow->n);
 break;
 }
-current = current->next;
+printf("[%p] %d\n", (void *)slow, slow->n);
+num_nodes++;
 }
-
 return (num_nodes);
 }
