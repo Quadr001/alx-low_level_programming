e248b9b661835e2e2288e7beac3d3e80c9796a87
Quadr <103822802+Quadr001@users.noreply.github.com>
2023-07-31 18:04:07 +0100

    101-print_listint_safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,8 +11,8 @@ size_t print_listint_safe(const listint_t *head);
 * Return: If the list is not looped - 0.
 * Otherwise - the number of unique nodes in the list.
 */
-size_t looped_listint_len(const listint_t *head)
 
+size_t looped_listint_len(const listint_t *head)
 {
 const listint_t *tortoise, *hare;
 size_t nodes = 1;
@@ -37,18 +37,20 @@ while (tortoise != hare)
 nodes++;
 tortoise = tortoise->next;
 }
-  return (nodes);
+return (nodes);
 }
-  tortoise = tortoise->next;
+tortoise = tortoise->next;
 hare = (hare->next)->next;
 }
 return (0);
 }
+
 /**
 * print_listint_safe - Prints a listint_t list safely.
 * @head: A pointer to the head of the listint_t list.
 * Return: The number of nodes in the list.
 */
+
 size_t print_listint_safe(const listint_t *head)
 {
 size_t nodes, index = 0;
