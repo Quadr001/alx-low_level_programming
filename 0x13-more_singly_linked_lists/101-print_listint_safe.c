066f340d9c48f41b1dd1a66d09307f9b7538a668
Quadr <103822802+Quadr001@users.noreply.github.com>
2023-07-31 21:37:12 +0100

    101-print_listint_safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,26 +3,28 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list
- * @head: Pointer to the head node of the linked list
- *
- * Return: The number of nodes in the list
- */
+* print_listint_safe - Prints a listint_t linked list and returns the number of nodes
+* @head: Pointer to the first node of the linked list
+*
+* Return: The number of nodes in the list
+*/
 size_t print_listint_safe(const listint_t *head)
 {
-size_t count = 0;
+size_t num_nodes = 0;
 const listint_t *current = head;
-const listint_t *check_loop = head;
+
 while (current)
 {
 printf("[%p] %d\n", (void *)current, current->n);
-count++;
-if (current->next >= check_loop)
+num_nodes++;
+/* Check if the next node points back to a previous node (infinite loop) */
+if (current >= current->next)
 {
 printf("-> [%p] %d\n", (void *)current->next, current->next->n);
 break;
 }
 current = current->next;
 }
-return (count);
+
+return (num_nodes);
 }
