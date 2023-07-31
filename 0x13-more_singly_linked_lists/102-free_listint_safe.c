1f78e0a7657f1c5c9607c456088eb06a263a68b7
Quadr <103822802+Quadr001@users.noreply.github.com>
2023-07-31 20:20:06 +0100

    102-free_listint_safe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,27 +1,29 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
-/**
- * print_listint_safe - Prints a listint_t linked list
- * @head: Pointer to the head node of the linked list
- *
- * Return: The number of nodes in the list
- */
-size_t print_listint_safe(const listint_t *head)
-{
-size_t count = 0;
-const listint_t *current = head;
-const listint_t *check_loop = head;
-while (current)
-{
-printf("[%p] %d\n", (void *)current, current->n);
-count++;
-if (current->next >= check_loop)
-{
-printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-break;
-}
-current = current->next;
-}
-return (count);
+size_t free_listint_safe(listint_t **h) {
+  listint_t *current = *h;
+  listint_t *tmp;
+  size_t count = 0;
+
+  if (h != NULL && *h != NULL) {
+    while (current != NULL) {
+      tmp = current->next;
+      free(current);
+      count++;
+      current = tmp;      
+
+      // Check for loop
+      for (listint_t *check = *h; check != NULL; check = check->next) {
+        if (check == current) {
+          *h = NULL;
+          return count;
+        }
+      }
+    }
+
+    *h = NULL;
+  }
+
+  return count;
 }
