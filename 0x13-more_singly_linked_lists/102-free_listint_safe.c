ae556d9420ef62500069174a4e065f788bfd26fa
Quadr <103822802+Quadr001@users.noreply.github.com>
2023-07-31 18:33:54 +0100

    102-free_listint_safe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -10,23 +10,19 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t count = 0;
-    const listint_t *current = head;
-    const listint_t *check_loop = head;
-
-    while (current)
-    {
-        printf("[%p] %d\n", (void *)current, current->n);
-        count++;
-
-        if (current->next >= check_loop)
-        {
-            printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-            break;
-        }
-
-        current = current->next;
-    }
-
-    return count;
+size_t count = 0;
+const listint_t *current = head;
+const listint_t *check_loop = head;
+while (current)
+{
+printf("[%p] %d\n", (void *)current, current->n);
+count++;
+if (current->next >= check_loop)
+{
+printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+break;
+}
+current = current->next;
+}
+return count;
 }
