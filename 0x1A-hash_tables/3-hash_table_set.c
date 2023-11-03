a8c53b6b8cd2286dceaa7e28e84c9722796e1e8d
Quadr <awequadr15@gmail.com>
2023-11-02 23:34:17 -0700

    hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -69,4 +69,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	hash_node->next = ht->array[index];
 	ht->array[index] = hash_node;
-	return (1);}
+	return (1);
+}
